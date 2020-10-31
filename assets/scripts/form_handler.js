cd35e13b14cecd8dd24e2dbded771a2e088eb038
Lance Avitia <lance77a@gmail.com>
2020-10-30 18:29:24 -0700

    95%. Deployable as is. Could clean the CSS up

--- assets/scripts/form_handler.js
+++ assets/scripts/form_handler.js
@@ -1,4 +1,4 @@
 const submit = document.querySelector("#submit");
-const message = "You submitted your form successfully! If this was your site, you'd be getting a notification email from a form handler.";
+const message = "You submitted this form successfully! If this was your site, you'd be getting a notification email from a form handler.";
 
 submit.addEventListener("click", () => alert(message));
